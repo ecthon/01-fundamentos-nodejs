549424e12aa2a31401e3a41fe8b1ef7ac779e517
ecthon <ecthon@gmail.com>
2024-01-17 15:06:31 -0400

    (Métodos HTTP) rotas de criação e listagem

--- src/server.js
+++ src/server.js
@@ -1,7 +1,28 @@
 import  http from 'node:http'
 
-// Criar um usuário (name, email, senha)
+// - HTTP
+//   - Método HTTP
+//   - URL
+
+// GET, POST, PUT, PATCH, DELETE
+
+// GET => Buscar um recurso do back-end
+// POST => Criar um recurso no back-end
+// PUT => Atualizar um recurso no back-end
+// PATCH => Atualizar uma informação específica de um recurso no back-end
+// DELETE => Deletar um recurso no back-end
+
 const server = http.createServer((req, res) => {
+  const { method, url } = req
+
+  if(method === 'GET' && url === '/users') {
+    return res.end('Listagem de usuários')
+  }
+
+  if(method === 'POST' && url === '/users') {
+    return res.end('Criação de usuário')
+  }
+
   return res.end('Hello World!')
 })
 
