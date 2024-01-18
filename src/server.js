1f6530f649317b82604917e8bf2f85f4efaecd1b
ecthon <ecthon@gmail.com>
2024-01-18 08:46:25 -0400

    Salvando usuários em memória (Headers)

--- src/server.js
+++ src/server.js
@@ -12,14 +12,27 @@ import  http from 'node:http'
 // PATCH => Atualizar uma informação específica de um recurso no back-end
 // DELETE => Deletar um recurso no back-end
 
+// JSON - JavaScript Object Notation
+
+// Cabeçalhos (Requisição/Respota) => Metadados
+
+const users = []
+
 const server = http.createServer((req, res) => {
   const { method, url } = req
 
   if(method === 'GET' && url === '/users') {
-    return res.end('Listagem de usuários')
+    return res
+    .setHeader('Content-type', 'application/json')
+    .end(JSON.stringify(users))
   }
 
   if(method === 'POST' && url === '/users') {
+    users.push({
+      id:1,
+      name: 'ecthon',
+      email: 'ecthon@email.com'
+    })
     return res.end('Criação de usuário')
   }
 
